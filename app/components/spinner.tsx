d7886d25d3bd6d41f0b9ecc637881a46b144f7b2
hiroshiimaizumi0611 <hiroshiimaizumi@gmail.com>
2024-09-03 11:32:41 +0900

    components

---
+++ app/components/spinner.tsx
@@ -0,0 +1,19 @@
+type Props = {
+  color?: string
+  width?: string
+  height?: string
+}
+
+export default function Spinner({
+  color = 'border-blue-500',
+  width = 'w-8',
+  height = 'h-8',
+}: Props) {
+  return (
+    <div className="m-2 flex justify-center">
+      <div
+        className={`${height} ${width} animate-spin rounded-full border-2 ${color} border-t-transparent`}
+      />
+    </div>
+  )
+}
