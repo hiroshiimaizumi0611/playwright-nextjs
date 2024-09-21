8cf63d7d72fdbfdafe94d7a3594a01ac8ef0cd78
hiroshiimaizumi0611 <hiroshiimaizumi@gmail.com>
2024-09-21 10:20:32 +0900

    task-item

--- app/components/task-item.tsx
+++ app/components/task-item.tsx
@@ -2,6 +2,8 @@ import useStore from '@/store'
 import { Task } from '@prisma/client'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { TrashIcon, PencilIcon } from '@heroicons/react/24/solid'
+import { taskIdSchema } from '@/schema/task'
 
 export default function TaskItem(task: Task) {
   const router = useRouter()
@@ -36,6 +38,24 @@ export default function TaskItem(task: Task) {
         }
       />
       <Link href={`task-crud/${task.id}`}>{task.title}</Link>
+      <div className="float-right ml-20 flex">
+        <PencilIcon
+          data-testid="test-edit-icon"
+          className="mx-1 h-5 w-5 cursor-pointer text-blue-500"
+          onClick={() =>
+            updatedEditedTask({
+              id: task.id,
+              title: task.title,
+              completed: task.completed,
+            })
+          }
+        />
+        <TrashIcon
+          data-testid="task-delete-icon"
+          className="h-5 w-5 cursor-pointer text-blue-500"
+          onClick={() => deleteTaskHandler(task.id)}
+        />
+      </div>
     </li>
   )
 }
